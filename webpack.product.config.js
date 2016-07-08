fe46264ce917f66ec72f6c3e64a8cad4abc9236d
ziaochina <ziaochina@gmail.com>
2016-07-08 18:09:58 +0800

    删除没用的

--- webpack.product.config.js
+++
@@ -1,6 +0,0 @@
-var config = require('./webpack.config.js');
-
-// config.output.path = __dirname + '/../publish/website/dist',
-config.output.path = "D:/Chanjet/TPlusPro/WebSite/dist";
-config.output.publicPath = '/tplus/dist/',
-module.exports = config;
